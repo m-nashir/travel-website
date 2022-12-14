de714a5d6f4bfe2406d0b0ff7513cf3edeb33308
Nashir <muh.nashir124@gmail.com>
2022-12-14 12:09:02 +0700

    all Done #Layout Responsive

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const navbar = document.querySelector('.navbar');
 window.addEventListener ('scroll', function(){
     navbar.classList.toggle ('sticky', window.scrollY > 0)
 })
+
+let menu = document.querySelector('#menu-icon');
+let menuNavbar = document.querySelector('.menu-navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-xmark')
+    menuNavbar.classList.toggle('open')
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-xmark')
+    menuNavbar.classList.remove('open')
+}
